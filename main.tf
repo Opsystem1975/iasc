49f5ec4c4865ffbf0e78d7abfb527770fae4c28d
Opsystem <opsystem@live.com.mx>
2022-09-17 11:58:55 -0500

    commit inicial

---
+++ main.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region     = "us-west-1"
+}
+
+resource "aws_instance" "Reverse-Proxy" {
+  instance_type          = "t2.micro"
+  ami                    = "ami-085284d24fe829cd0"
+  key_name               = "MRIS"
+  tags = {
+    Name = "OPSYSTEM"
+  }
+  vpc_security_group_ids = [aws_security_group.WebSG.id]
+  user_data              = filebase64("${path.module}/scripts/docker.sh")
+}
+
+resource "aws_security_group" "WebSG" {
+  name = "sg_reglas_firewall"
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SG HTTPS"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SG HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+  }
+  ingress {
+    cidr_blocks = ["187.189.168.51/32"]
+    description = "SG SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SG All trafic Outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+  } 
+}
+
+output "public_ip" {
+  value = "${join(",", aws_instance.Reverse-Proxy.*.public_ip)}"
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "AWS_ACCESS_KEY"{
+    type=string
+    description = "My AWS Access key"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+    type=string
+    description = "My AWS Secret access Key"
+}
